e7736afa9cf50f6a298967a4253383577616491e
ammar yasser <ammarasc74@gmail.com>
2021-07-22 21:04:51 +0200

    feat: adding user model to db.

---
+++ src/models/users/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: {
+        type: String,
+        requried: true,
+    },
+
+    email: {
+        type: String,
+        requried: true,
+        uniqe: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
