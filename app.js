c1c8a4bd9494bac0dd6eb00971e46580932585cd
ammar yasser <ammarasc74@gmail.com>
2021-07-26 14:57:58 +0200

    fix: handling node module file.

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use('/user', userRoutes)
 
 //  error handling here
 
-app.use((next) => {
+app.use((req, res, next) => {
     const error = new Error('Not Found');
     error.status = 404;
     next(error);
